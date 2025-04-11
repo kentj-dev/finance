3f620b875426b27bf5783440fb90ad39e9c5059e
Kent <kentjamila@gmail.com>
2025-04-11 20:49:33 +0800

    user component

---
+++ resources/js/components/dashboard/user.tsx
@@ -0,0 +1,38 @@
+import { Auth, User } from '@/types';
+import { Link } from '@inertiajs/react';
+import { router } from '@inertiajs/react';
+
+export function UserList({ user, auth }: { user: User; auth: Auth }) {
+
+    const deleteUser = (userId: number) => {
+        if (confirm('Are you sure you want to delete this user?')) {
+            router.delete(route('dashboard.delete-user', userId), {
+                preserveScroll: true,
+                onSuccess: () => {
+                    alert('User deleted successfully!');
+                },
+                onError: (errors) => {
+                    alert('Failed to delete user.' + errors.message);
+                },
+            });
+        }
+    };
+
+    return (
+        <div key={user.id} className="rounded-md border p-4">
+            <h2 className="text-lg font-semibold">{user.name}</h2>
+            <p className="text-gray-7500 text-xs">{user.email}</p>
+            <p className="text-xs text-gray-500">{user.id}</p>
+            <p className="flex items-center gap-2 text-xs">
+                <Link href={route('dashboard.view-user', user.id)} className="text-blue-600 hover:underline">
+                    View
+                </Link>
+                {auth.user.id !== user.id && (
+                    <button className="text-red-600 hover:underline" onClick={() => deleteUser(user.id)}>
+                        Delete
+                    </button>
+                )}
+            </p>
+        </div>
+    );
+}
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { UserList } from '@/components/dashboard/user';
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
@@ -5,7 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/app-layout';
 import { SharedData, User, type BreadcrumbItem } from '@/types';
-import { Head, Link, router, useForm, usePage } from '@inertiajs/react';
+import { Head, router, useForm, usePage } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
 import { FormEventHandler, useEffect, useState } from 'react';
 import { toast } from 'sonner';
@@ -68,20 +69,6 @@ export default function Dashboard({ users, filters }: DashboardProps) {
         });
     };
 
-    const deleteUser = (userId: number) => {
-        if (confirm('Are you sure you want to delete this user?')) {
-            router.delete(route('dashboard.delete-user', userId), {
-                preserveScroll: true,
-                onSuccess: () => {
-                    alert('User deleted successfully!');
-                },
-                onError: (errors) => {
-                    alert('Failed to delete user.' + errors.message);
-                },
-            });
-        }
-    };
-
     useEffect(() => {
         const delayDebounce = setTimeout(() => {
             const query = searchParam.trim() ? { search: searchParam } : {};
@@ -109,21 +96,7 @@ export default function Dashboard({ users, filters }: DashboardProps) {
                 {users.length > 0 ? (
                     <div className="grid gap-2">
                         {users.map((user) => (
-                            <div key={user.id} className="rounded-md border p-4">
-                                <h2 className="text-lg font-semibold">{user.name}</h2>
-                                <p className="text-gray-7500 text-xs">{user.email}</p>
-                                <p className="text-xs text-gray-500">{user.id}</p>
-                                <p className="flex items-center gap-2 text-xs">
-                                    <Link href={route('dashboard.view-user', user.id)} className="text-blue-600 hover:underline">
-                                        View
-                                    </Link>
-                                    {auth.user.id !== user.id && (
-                                        <button className="text-red-600 hover:underline" onClick={() => deleteUser(user.id)}>
-                                            Delete
-                                        </button>
-                                    )}
-                                </p>
-                            </div>
+                            <UserList user={user} auth={auth}/>
                         ))}
                     </div>
                 ) : (
